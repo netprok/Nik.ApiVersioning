c3784e0215e2c3ae77ad9ec8b717e98e46b0853e
Ehsan Faramarzi <ehsan.faramarzi@turck.com>
2023-11-05 17:19:41 +0100

    Add project files.

---
+++ Nik.ApiVersioning/ApiVersioningHelper.cs
@@ -0,0 +1,45 @@
+﻿namespace Nik.ApiVersioning;
+
+public static class ApiVersioningHelper
+{
+    public static void UseAppVersioning(WebApplicationBuilder builder)
+    {
+        builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.OperationFilter<SwaggerDefaultValues>();
+        });
+
+        builder.Services
+            .AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+            })
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+    }
+
+    public static void UseSwaggerUi(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                var descriptions = app.DescribeApiVersions();
+
+                // Build a swagger endpoint for each discovered API version
+                foreach (var description in descriptions)
+                {
+                    var url = $"/swagger/{description.GroupName}/swagger.json";
+                    var name = description.GroupName.ToUpperInvariant();
+                    options.SwaggerEndpoint(url, name);
+                }
+            });
+        }
+    }
+}
---
+++ Nik.ApiVersioning/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using Asp.Versioning.ApiExplorer;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Options;
+global using Microsoft.OpenApi.Models;
+global using Swashbuckle.AspNetCore.SwaggerGen;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.Hosting;
