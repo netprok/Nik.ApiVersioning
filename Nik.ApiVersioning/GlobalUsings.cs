cde74ce9dc99ad5415ba4edb0724f26e3b2a51a0
Ehsan Faramarzi <netpro@email.com>
2024-04-14 08:56:08 +0200

    pk up

--- Nik.ApiVersioning/GlobalUsings.cs
+++ Nik.ApiVersioning/GlobalUsings.cs
@@ -1,13 +1,17 @@
 global using Asp.Versioning.ApiExplorer;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Options;
-global using Microsoft.OpenApi.Models;
-global using Swashbuckle.AspNetCore.SwaggerGen;
+
 global using Microsoft.AspNetCore.Builder;
-global using Microsoft.Extensions.Hosting;
-global using System;
 global using Microsoft.AspNetCore.Mvc.ApiExplorer;
 global using Microsoft.AspNetCore.Mvc.ModelBinding;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Options;
+global using Microsoft.OpenApi.Models;
+
+global using Nik.ApiVersioning.Swagger;
+
+global using Swashbuckle.AspNetCore.SwaggerGen;
+
+global using System;
 global using System.Linq;
 global using System.Text.Json;
-global using Nik.ApiVersioning.Swagger;
